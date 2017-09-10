83989780e825b7213cccab666bf5413de426ed61
tz70s <su3g4284zo6y7@gmail.com>
2017-09-10 12:15:09 +0800

    restruct thee enumeration

--- vm.h
+++ vm.h
@@ -16,45 +16,15 @@ int *pc, *bp, *sp, ax, cycle;
 
 /* instructions and AT&T style */
 enum {
-    LEA,
-    IMM, /* Put data into ax */
-    JMP,
-    CALL,
-    JZ,
-    JNZ,
-    ENT,
-    ADJ,
-    LEV,
+    LEA, IMM, /* Put data into ax */
+    JMP, CALL, JZ, JNZ, ENT, ADJ, LEV,
     LI, /* Put the address of integer into ax */
     LC, /* Put the address of char into ax */
     SI, /* Store the integer of ax into stack */
     SC, /* Store the character of ax into stack */
-    PUSH,
-    OR,
-    XOR,
-    AND,
-    EQ,
-    NE,
-    LT,
-    GT,
-    LE,
-    GE,
-    SHL,
-    SHR,
-    ADD,
-    SUB,
-    MUL,
-    DIV,
-    MOD,
+    PUSH, OR, XOR, AND, EQ, NE, LT, GT, LE, GE, SHL, SHR, ADD, SUB, MUL, DIV, MOD,
     /* c functions */
-    OPEN,
-    READ,
-    CLOS,
-    PRTF,
-    MALC,
-    MSET,
-    MCMP,
-    EXIT
+    OPEN, READ, CLOS, PRTF, MALC, MSET, MCMP, EXIT
 };
 
 void vm_init();
